214d805a3d5f0cea084864d36526740daa092cb3
SAURABH YADAV <55534242+saurabh931@users.noreply.github.com>
2023-12-19 10:29:51 +0800

    change path of service

--- app/services/page.js
+++ app/services/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Service from '../../Component/pages/Service'
 
 export const metadata = {
-    title: "Service | Welcome to Ai Green Bubble",
+    title: "Services | Welcome to Ai Green Bubble",
     description: "Ai Green Bubble  we are at the forefront of revolutionary business operations driving the evolution of blockchain technology Web3 solutions and Islamic Fintech decentralized applications",
     openGraph: {
       images: [
